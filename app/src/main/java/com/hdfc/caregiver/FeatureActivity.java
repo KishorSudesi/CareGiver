19c96f312988cdded84f2994fc080f7c76d5581b
Aniket <aniket@sudesi.in>
2016-05-04 16:13:05 +0530

    Expandable list view

--- app/src/main/java/com/hdfc/caregiver/FeatureActivity.java
+++ app/src/main/java/com/hdfc/caregiver/FeatureActivity.java
@@ -22,12 +22,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.hdfc.adapters.ExpandableListAdapter;
 import com.hdfc.adapters.FeatureAdapter;
 import com.hdfc.app42service.StorageService;
 import com.hdfc.app42service.UploadService;
@@ -57,6 +59,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 
 public class FeatureActivity extends AppCompatActivity implements Serializable{
@@ -85,6 +88,13 @@ public class FeatureActivity extends AppCompatActivity implements Serializable{
     private Point p;
     private JSONArray jsonArrayImagesAdded;
 
+    //Expandable listview Adapter
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
  private static ArrayList<String> imagePaths = new ArrayList<>();
     private static ArrayList<Bitmap> bitmaps = new ArrayList<>();
     @Override
@@ -96,7 +106,7 @@ public class FeatureActivity extends AppCompatActivity implements Serializable{
         ImageView imgLogoHeaderTaskDetail = (ImageView) findViewById(R.id.imgLogoHeaderTaskDetail);
         Button done = (Button) findViewById(R.id.buttonVegetibleDone);
         ImageView back = (ImageView) findViewById(R.id.imgBackHeaderTaskDetail);
-        ListView featuresList = (ListView) findViewById(R.id.list_view);
+        //ListView featuresList = (ListView) findViewById(R.id.list_view);
         //surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
         TextView textViewEmpty = (TextView) findViewById(android.R.id.empty);
         TextView textViewTime = (TextView) findViewById(R.id.textViewTime);
@@ -205,12 +215,12 @@ public class FeatureActivity extends AppCompatActivity implements Serializable{
         }*/
 
 
- if (featuresList != null) {
+/* if (featuresList != null) {
         featuresList.setAdapter(featureAdapter);
  }
 if (featuresList != null) {
         featuresList.setEmptyView(textViewEmpty);
- }
+ }*/
 
   if (back != null) {
         back.setOnClickListener(new View.OnClickListener() {
@@ -240,8 +250,65 @@ if (attach != null) {
 			}
         });
      }
-    
+
+
+        //Expandable Listview
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
     }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
+
+
 
     // The method that displays the popup.
     private void showStatusPopup(final Activity context, Point p) {
