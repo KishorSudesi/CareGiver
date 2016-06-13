319e49274b024198a87b30ccb54a80b6a5b965f5
Aniket <aniket@sudesi.in>
2016-06-13 13:10:47 +0530

    New Changes

--- app/src/main/java/com/hdfc/libs/AppUtils.java
+++ app/src/main/java/com/hdfc/libs/AppUtils.java
@@ -1901,7 +1901,7 @@ public class AppUtils {
     public void fetchClients(final int iFlag) {
 
         if (utils.isConnectingToInternet()) {
-
+            fetchCustomers(2);
             StorageService storageService = new StorageService(_ctxt);
 
             Query q1 = QueryBuilder.build("provider_id", Config.providerModel.getStrProviderId(),
